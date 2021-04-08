fb92d75c534203ccce2c9024eef51bde8b8873ed
Amar Kumar Ram <ram.amarkumar@gmail.com>
2021-04-08 16:34:27 +0530

    10th session

--- first-project/src/functional-component/HooksStateObject.js
+++ first-project/src/functional-component/HooksStateObject.js
@@ -2,23 +2,28 @@ import React, {useState} from 'react'
 
 function HooksStateObject() {
 
-    let empObj = {
-        name: "ABC",
-        company: "TCS"
+    let numArr = [1, 2, 3];
+
+    const [currentArr, updateObj] = useState(numArr);
+
+    let appendNum = () => {
+        const x = Math.floor(Math.random() * 11);
+        updateObj([...currentArr, x]); //[1,2,3] + New number
+        console.log(currentArr);
     }
 
-    const [currentObj, updateObj] = useState(empObj)
-
     return(
-        <form>
-            <input type="text" value={currentObj.name} onChange={(e) => updateObj({name: e.target.value})} />
-            <input type="text" value={currentObj.company} onChange={(e) => updateObj({company: e.target.value})} />
-            <br/>
-            Employee Name: {currentObj.name}<br/>
-            Company: {currentObj.company}<br />
-            //Spread Operator in Javascript
-        </form>
+        <>
+            {currentArr}<br />
+            <button onClick={appendNum}>Append Number</button>
+        </>
     )
+
+    /**
+     * Difference between class component setState method and functional component useState hook
+     * 1. setState update and merge objects automatically whereas developer needs to merge objects/array in useState manually
+     * 2. setState is ALWAYS an object whereas useState can be a string, number, boolean, array, objects
+     */
 }
 
 export default HooksStateObject;
